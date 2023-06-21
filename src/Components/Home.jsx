6c6f1c078cd26e1122f9a8e8e9eb7c7655a4ba87
PkTheCoda <pranavkonjeti@gmail.com>
2023-06-21 10:26:38 -0400

    added watermark (my name/github profile)

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Header from "./Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
     return (
@@ -11,7 +12,9 @@ function Home() {
                     <div className="home--description">Urban Escape is your gateway to a tranquil retreat in the heart of the city. Discover a curated collection of stylish furniture and decor, meticulously designed to transform your urban space into a sanctuary of relaxation and rejuvenation.</div>
                     <NavLink to="/store"><button className="home-store-button">Visit the Store</button></NavLink>
                 </div>
+                <div className="watermark">Made with ❤️ by <a href="https://github.com/PkTheCoda"><span className="watermark-name">Pranav</span></a></div>
             </div>
+
             
         </>
     )
